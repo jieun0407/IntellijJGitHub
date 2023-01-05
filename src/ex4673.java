8127beeceebb949c28eb2f4a052cbba4919ceba3
imjieun <jieun3604@gmail.com>
2023-01-05 22:09:29 +0900

    ex4673-2

--- src/ex4673.java
+++ src/ex4673.java
@@ -1,12 +1,34 @@
 public class ex4673 {
     public static void main(String[] args) {
-        for(int i =1; i<=1000; i++){
+        int arr[] = new int[10000];
+        for(int i =1; i<=10000; i++){
             int n =d(i);
+            arr[i-1]=n;
+        }
+        int arr2[] = new int[10000];
+        for(int i=1;i<=10000;i++){
+            boolean result = false;
+            for(int a: arr){
+                if(a==i){
+
+                    result = true;
+                }
+            }
+
+            if(result==false){
+
+                arr2[i++] = i;
+
+            }
+        }
+
+        for(int b: arr2){
+            System.out.println(b);
         }
     }
 
     public static int d(int number){
-
-        return 1;
+        int n = number + number/100 + number/10%10 + number%10;
+        return n;
     }
 }

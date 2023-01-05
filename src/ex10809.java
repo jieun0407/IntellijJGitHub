ee0b0b1d758a61872e14a821b138b4657a07a7df
imjieun <jieun3604@gmail.com>
2023-01-06 00:18:01 +0900

    ex10809-1

---
+++ src/ex10809.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ex10809 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = br.readLine();
+        int arr[] = new int[26];
+
+        for(char ch = 'a'; ch<='z'; ch++){
+
+            for(int i =0; i<S.length();i++){
+                //char str = (S.charAt(i));
+                if(ch==S.charAt(i)){
+                    arr[i]=
+                }
+            }
+        }
+    }
+}

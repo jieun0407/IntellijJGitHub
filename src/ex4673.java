6a00b24fe721a1a9eea5b049982e3e7f0960d586
imjieun <jieun3604@gmail.com>
2023-01-05 17:38:49 +0900

    ex4673-1

---
+++ src/ex4673.java
@@ -0,0 +1,12 @@
+public class ex4673 {
+    public static void main(String[] args) {
+        for(int i =1; i<=1000; i++){
+            int n =d(i);
+        }
+    }
+
+    public static int d(int number){
+
+        return 1;
+    }
+}

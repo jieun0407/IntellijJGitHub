82b4a008fb6d05edc85c773dd76d386eddec3198
imjieun <jieun3604@gmail.com>
2023-01-05 22:37:40 +0900

    ex4673-3

--- src/ex4673.java
+++ src/ex4673.java
@@ -1,34 +1,33 @@
 public class ex4673 {
     public static void main(String[] args) {
-        int arr[] = new int[10000];
-        for(int i =1; i<=10000; i++){
+        boolean[] check = new boolean[10001];
+        for(int i =1; i<10001; i++){
             int n =d(i);
-            arr[i-1]=n;
-        }
-        int arr2[] = new int[10000];
-        for(int i=1;i<=10000;i++){
-            boolean result = false;
-            for(int a: arr){
-                if(a==i){
-
-                    result = true;
-                }
+            if(n<10001){
+                check[n] = true;
             }
+        }
 
-            if(result==false){
+        //StringBuilder sb = new StringBuilder();
 
-                arr2[i++] = i;
+        for(int i=1; i<10001; i++){
+            if(!check[i]){
+                System.out.println(i);
 
             }
         }
 
-        for(int b: arr2){
-            System.out.println(b);
-        }
     }
 
     public static int d(int number){
-        int n = number + number/100 + number/10%10 + number%10;
-        return n;
+
+        //int n = number + number/100 + number/10%10 + number%10;
+        int sum = number;
+
+        while(number!=0){
+            sum = sum + (number%10); //첫째자리수
+            number = number/10; //10을 나누어 첫째자리를 없앤다
+        }
+        return sum;
     }
 }
